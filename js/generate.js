e9096d39e5e120bc80dc2e94e7818f24c2bab20d
Pozzitive11 <palaga2442@gmail.com>
2022-08-07 21:24:50 +0300

    fix

--- js/generate.js
+++ js/generate.js
@@ -1,3 +1,148 @@
+// const cards = [
+//   {
+//     front: "images/front.jpg",
+//     back: "images/sleepy-cat.jpg",
+//     name: "sleepy",
+//   },
+//   {
+//     front: "images/front.jpg",
+//     back: "images/cat.jpg",
+//     name: "cat",
+//   },
+//   {
+//     front: "images/front.jpg",
+//     back: "images/frightened-cat.jpg",
+//     name: "frightened",
+//   },
+//   {
+//     front: "images/front.jpg",
+//     back: "images/brutal-cat.jpg",
+//     name: "brutal",
+//   },
+//   {
+//     front: "images/front.jpg",
+//     back: "images/rock-cat.jpg",
+//     name: "rock",
+//   },
+//   {
+//     front: "images/front.jpg",
+//     back: "images/cute-cat.jpg",
+//     name: "cute",
+//   },
+// ];
+
+// const createCard = (front, back, name) => {
+//   return `<div class="game__card" id="${name}">
+//             <img class="front__img" src="${front}" alt="">
+//             <img class="back__img" src="${back}" alt="">
+//           </div>
+//           <div class="game__card" id="${name}">
+//             <img class="front__img" src="${front}" alt="">
+//             <img class="back__img" src="${back}" alt="">
+//           </div>`;
+// };
+
+// const gameCard = cards.map(({ front, back, name }) =>
+//   createCard(front, back, name)
+// );
+// const gameContainer = document.querySelector(".game__container");
+// gameContainer.innerHTML = gameCard.join("");
+// const gameCards = document.querySelectorAll(".game__card");
+
+// let numberOfMatches = 0;
+// let arrayOfFlippedCards = [];
+
+// gameCards.forEach((card) =>
+//   card.addEventListener("click", ({ target }) => {
+//     const parentTarget = target.parentElement;
+//     if (parentTarget && parentTarget.classList.contains("game__card")) {
+//       parentTarget.classList.add("flip");
+
+//       arrayOfFlippedCards.push(parentTarget);
+//       console.log(arrayOfFlippedCards);
+//     }
+//     checkArrayOfFlippedCardsLength();
+//     checkForWin();
+//   })
+// );
+
+// function checkArrayOfFlippedCardsLength() {
+//   if (arrayOfFlippedCards.length % 2 === 0) {
+//     checkMatch();
+//   }
+// }
+
+// function checkMatch() {
+//   // const match =
+//   //   arrayOfFlippedCards[arrayOfFlippedCards.length - 1].id ===
+//   //   arrayOfFlippedCards[arrayOfFlippedCards.length - 2].id;
+
+//   // if (match) {
+//   //   numberOfMatches++;
+//   //   addingDisableClass();
+//   //   setTimeout(() => {
+//   //     addingDisableClass();
+//   //   }, 1000);
+//   // } else {
+//   //   removeFlip();
+//   // }
+
+//   if (
+//     arrayOfFlippedCards[arrayOfFlippedCards.length - 1].id ===
+//     arrayOfFlippedCards[arrayOfFlippedCards.length - 2].id
+//   ) {
+//     numberOfMatches++;
+//   } else {
+//     removeFlip();
+//     shuffleCards();
+//   }
+// }
+
+// function removeFlip() {
+//   setTimeout(() => {
+//     arrayOfFlippedCards[arrayOfFlippedCards.length - 1].classList.remove(
+//       "flip"
+//     );
+//     arrayOfFlippedCards[arrayOfFlippedCards.length - 2].classList.remove(
+//       "flip"
+//     );
+//     arrayOfFlippedCards.splice(0, 2);
+
+//     addingDisableClass();
+
+//     checkForWin();
+//   }, 100);
+//   addingDisableClass();
+// }
+
+// function addingDisableClass() {
+//   gameCards.forEach((item) => {
+//     item.classList.toggle("disableclick");
+//   });
+// }
+
+// function checkForWin() {
+//   if (arrayOfFlippedCards.length === 12) {
+//     setTimeout(() => {
+//       gameCards.forEach((item) => {
+//         item.classList.remove("flip");
+//       });
+//       alert("U WIN!");
+//       arrayOfFlippedCards.splice(0, arrayOfFlippedCards.length);
+//       // shuffleCards();
+//     }, 100);
+//   }
+// }
+
+// function shuffleCards() {
+//   gameCards.forEach((card) => {
+//     let randomPos = Math.floor(Math.random() * 12);
+//     card.style.order = randomPos;
+//   });
+// }
+
+// shuffleCards();
+
 const cards = [
   {
     front: "images/front.jpg",
@@ -49,34 +194,39 @@ const gameContainer = document.querySelector(".game__container");
 gameContainer.innerHTML = gameCard.join("");
 const gameCards = document.querySelectorAll(".game__card");
 
-let counter = 0;
-let arr = [];
+let numberOfMatches = 0;
+let arrayOfFlippedCards = [];
 
-function flipCard() {
-  if (this && this.classList.contains("game__card")) {
-    this.classList.add("flip");
+const cardd = document.querySelector(".game__card");
 
-    arr.push(this);
-  }
-  checkArrLength();
+gameCards.forEach((card) =>
+  card.addEventListener("click", ({ target }) => {
+    const parentTarget = target.closest(".game__card");
+    if (parentTarget && parentTarget.classList.contains("game__card")) {
+      parentTarget.classList.add("flip");
 
-  winCOndition();
-}
+      arrayOfFlippedCards.push(parentTarget);
+    }
+    checkArrayOfFlippedCardsLength();
+    checkForWin();
+  })
+);
 
-function checkArrLength() {
-  if (arr.length % 2 === 0) {
+function checkArrayOfFlippedCardsLength() {
+  if (arrayOfFlippedCards.length === 2) {
     checkMatch();
   }
 }
 
 function checkMatch() {
-  const match = arr[arr.length - 1].id === arr[arr.length - 2].id;
+  const match = arrayOfFlippedCards[0].id === arrayOfFlippedCards[1].id;
 
   if (match) {
-    counter++;
-    disableClick();
+    numberOfMatches++;
+    addingDisableClass();
+    arrayOfFlippedCards.splice(0, arrayOfFlippedCards.length);
     setTimeout(() => {
-      disableClick();
+      addingDisableClass();
     }, 1000);
   } else {
     removeFlip();
@@ -85,43 +235,42 @@ function checkMatch() {
 
 function removeFlip() {
   setTimeout(() => {
-    arr[arr.length - 1].classList.remove("flip");
-    arr[arr.length - 2].classList.remove("flip");
-    arr.splice(0, 2);
+    arrayOfFlippedCards[0].classList.remove("flip");
+    arrayOfFlippedCards[1].classList.remove("flip");
+    arrayOfFlippedCards = [];
 
-    disableClick();
+    addingDisableClass();
 
-    winCOndition();
+    checkForWin();
   }, 1000);
-  disableClick();
+  addingDisableClass();
 }
 
-function disableClick() {
+function addingDisableClass() {
   gameCards.forEach((item) => {
     item.classList.toggle("disableclick");
   });
 }
 
-function winCOndition() {
-  if (arr.length === 12) {
+function checkForWin() {
+  if (numberOfMatches === 6) {
     setTimeout(() => {
       gameCards.forEach((item) => {
         item.classList.remove("flip");
       });
       alert("U WIN!");
-      arr.splice(0, arr.length);
-      shuffle();
+      arrayOfFlippedCards.splice(0, arrayOfFlippedCards.length);
+      shuffleCards();
+      numberOfMatches = 0;
     }, 1000);
   }
 }
 
-function shuffle() {
+function shuffleCards() {
   gameCards.forEach((card) => {
     let randomPos = Math.floor(Math.random() * 12);
     card.style.order = randomPos;
   });
 }
 
-shuffle();
-
-gameCards.forEach((card) => card.addEventListener("click", flipCard));
+shuffleCards();
